780bb5406121bca1fee5de17007a15d689ecfce5
Chad Woolley <thewoolleyman@gmail.com>
2017-04-12 00:51:32 -0600

    download rancher-compose

--- ci/set-env-vars.sh
+++ ci/set-env-vars.sh
@@ -6,11 +6,13 @@ DOCKER_REPO_NAME=docker-webserver-example
 DOCKER_SEMVER_MAJOR_MINOR_PATCH_VERSION=0.0.1
 
 # Rancher
-RANCHER_ENV=default
 COMPOSE_PROJECT_NAME=example-stack # Stack Name
+RANCHER_ENV=default
+COMPOSE_FILE=rancher/$RANCHER_ENV/$COMPOSE_PROJECT_NAME/docker-compose.yml
+RANCHER_COMPOSE_VERSION=0.12.4
 RANCHER_LB_NAME=example-load-balancer
 RANCHER_URL=http://rancher.illumin8.us:8080
-COMPOSE_FILE=rancher/$RANCHER_ENV/$COMPOSE_PROJECT_NAME/docker-compose.yml
+
 
 # Secrets for local testing
 if [ -e ~/.rancher/${RANCHER_ENV}/api-keys ]; then
